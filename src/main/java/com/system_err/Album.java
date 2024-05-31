8fe68b388b282292ff8bfdb1b644261e32185385
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-05-30 17:36:47 -0700

    Finished challenge

--- src/main/java/com/system_err/Album.java
+++ src/main/java/com/system_err/Album.java
@@ -2,6 +2,7 @@ package com.system_err;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 public class Album {
@@ -20,25 +21,46 @@ public class Album {
         songs.add(song);
     }
 
-    private Song findSong(String title) {
-        for (Song song : songs) {
-            if (Objects.equals(song.getTitle(), title)) {
-                return song;
-            }
-        }
-
-        return null;
-    }
-
     public void addToPlayList(int track, LinkedList<Song> playlist) {
-        int index = track - 1;
-        if (index >= 0 && index < songs.size()) {
-            playlist.add(songs.get(track - 1));
-        }
+        playlist.add(new SongList(songs).findSong(track));
     }
 
     public void addToPlayList(String title, LinkedList<Song> playlist) {
-        Song song = findSong(title);
-        playlist.add(song);
+        playlist.add(new SongList(songs).findSong(title));
+    }
+
+    public static class SongList {
+        private static List<Song> songs;
+
+        private SongList(List<Song> songs) {
+            SongList.songs = songs;
+        }
+        
+        private void add(Song song) {
+            if (!songs.contains(song)) {
+                songs.add(song);
+            }
+        }
+
+        private Song findSong(String title) {
+            for (Song song : songs) {
+                if (Objects.equals(song.getTitle(), title)) {
+                    return song;
+                }
+            }
+
+            System.out.println("The song " + title + " is not in this album");
+            return null;
+        }
+        
+        private Song findSong(int track) {
+            int index = track - 1;
+            if (index >= 0 && index < songs.size()) {
+                return songs.get(index);
+            }
+
+            System.out.println("This album does not have a track " + track);
+            return null;
+        }
     }
 }
